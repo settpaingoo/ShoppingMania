53f80ec2855a2707233b022d21067fe7c06be5bc
App Academy Student <student@appacademy.io>
2014-02-17 12:33:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingMania::Application.config.secret_token = 'cc42d504d9222db55097255e7901ba8a42861c6f64e7444962b8750c589ecb2e0dfae97632429712843b64e65f10ed70d1ce500e0865b409ba690658363aace5'

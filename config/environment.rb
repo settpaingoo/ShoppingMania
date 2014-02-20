2266f30dd6e3fb172502e8f869b416142809b60d
Sett Paing Oo <oo.settpaing@gmail.com>
2014-02-20 18:19:26 -0500

    Config for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,15 @@ require File.expand_path('../application', __FILE__)
 ShoppingMania::Application.initialize!
 
 if Rails.env.production?
-
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+    }
+  ActionMailer::Base.delivery_method ||= :smtp
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
 end
